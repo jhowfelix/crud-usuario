ec63fca38228a3700b00122514f7bd435a81ef18
Jonathan Felix <jhow.wi56@gmail.com>
2022-08-22 20:01:53 -0300

    feito estrutura do projeto

---
+++ src/main/java/br/com/fiap/controller/UsuarioController.java
@@ -0,0 +1,10 @@
+package br.com.fiap.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class UsuarioController {
+
+}
