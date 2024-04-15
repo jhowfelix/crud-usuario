f957b0fde0c03e331ea6c40f2d58ca143e28ea4f
Jonathan Felix <jhow.wi56@gmail.com.com>
2024-04-15 13:51:17 -0300

    adicionado tratamento de exceções

--- src/main/java/br/com/fiap/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/controller/exceptions/ControllerExceptionHandler.java
@@ -4,12 +4,15 @@ import java.time.LocalDateTime;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import br.com.fiap.service.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -22,5 +25,23 @@ public class ControllerExceptionHandler {
 		return ResponseEntity.status(status).body(error);
 
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> badRequest(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(LocalDateTime.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<StandardError> badRequest(EmptyResultDataAccessException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(LocalDateTime.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+
+    }
+
+    
 
 }
--- src/main/java/br/com/fiap/service/UsuarioService.java
+++ src/main/java/br/com/fiap/service/UsuarioService.java
@@ -10,41 +10,38 @@ import br.com.fiap.dto.UsuarioDto;
 import br.com.fiap.mapper.UsuarioMapper;
 import br.com.fiap.model.Usuario;
 import br.com.fiap.repository.UsuarioRepository;
-
+import br.com.fiap.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	UsuarioRepository repository;
-	
 
 	@Autowired
 	UsuarioMapper mapper;
 
-
-
-	
 	public UsuarioDto findById(Long id) {
 
 		Optional<Usuario> usuarioEntity = repository.findById(id);
+		usuarioEntity.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado!"));
 		UsuarioDto dto = mapper.toDto(usuarioEntity.get());
-		return  dto;
+		return dto;
 	}
-	
-	public List<UsuarioDto> findAll(){
-		List<UsuarioDto> dtos = mapper.toDtos( repository.findAll());
-	
+
+	public List<UsuarioDto> findAll() {
+		List<UsuarioDto> dtos = mapper.toDtos(repository.findAll());
+
 		return dtos;
 	}
-	
+
 	public UsuarioDto insert(UsuarioDto usuarioDto) {
 		Usuario entity = mapper.toEntity(usuarioDto);
 		Usuario saveUsuario = repository.save(entity);
 		return mapper.toDto(saveUsuario);
-		
+
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
